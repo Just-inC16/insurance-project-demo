473774bf8ba91a685a28250579032f5931b4b8d5
Justin Chan <justin.chan.3719@gmail.com>
2023-11-04 20:00:13 -0400

    Adding unit test + edit to pom.xml

--- src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
+++ src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
@@ -1,9 +1,6 @@
 package com.project.staragile.insureme;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -11,26 +8,25 @@ class InsureMeApplicationTests {
 	@Test
 	void testCreationMessage() throws InterruptedException {
 		System.out.println("Contact Page");
-//		WebDriverManager.chromedriver().setup();
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		ChromeOptions chromeOptions = new ChromeOptions();
-		WebDriver webdriver = new ChromeDriver();
+//		ChromeOptions chromeOptions = new ChromeOptions();
+//		WebDriver webdriver = new ChromeDriver();
 
 		// Navigate to contact page
-//	String contactUrl2 = "http://localhost:8083/contact.html";
-//	String contactUrl = serviceBaseUrl + "/contact.html";
-//	webdriver.get(contactUrl2);
+//		String contactUrl2 = "http://localhost:8083/contact.html";
+//		String contactUrl = serviceBaseUrl + "/contact.html";
+//		webdriver.get(contactUrl2);
 
 		// Populate the contact page to determine if we are able to send information
-//	webdriver.findElement(By.id("inputName")).sendKeys("Justin");
-//	webdriver.findElement(By.id("inputNumber")).sendKeys("123456786");
-//	webdriver.findElement(By.id("inputMail")).sendKeys("face@gmail.com");
-//	webdriver.findElement(By.id("inputMessage")).sendKeys("Hello. I'm not a bot.");
-//	webdriver.findElement(By.id("my-button")).click();
+//		webdriver.findElement(By.id("inputName")).sendKeys("Justin");
+//		webdriver.findElement(By.id("inputNumber")).sendKeys("123456786");
+//		webdriver.findElement(By.id("inputMail")).sendKeys("face@gmail.com");
+//		webdriver.findElement(By.id("inputMessage")).sendKeys("Hello. I'm not a bot.");
+//		webdriver.findElement(By.id("my-button")).click();
 
 		// Check if "Message Sent"
-//	String responseText = webdriver.findElement(By.id("response")).getText();
-//	TimeUnit.SECONDS.sleep(4);
-//	Assert.assertEquals("Message Sent", responseText);
+//		String responseText = webdriver.findElement(By.id("response")).getText();
+//		TimeUnit.SECONDS.sleep(4);
+//		Assert.assertEquals("Message Sent", responseText);
 	}
 }
